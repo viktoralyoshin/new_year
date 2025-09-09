d7fb3e379821572ff619024f38b97b670573028c
viktoralyoshin <viktor.aleshin2004@gmail.com>
2025-09-09 10:28:25 +0800

    final

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	newYear := time.Date(2026, time.January, 1, 0, 0, 0, 0, time.Local)
+
+	until := time.Until(newYear).Hours() / 24
+
+	fmt.Println(int(until))
+}
